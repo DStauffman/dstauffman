4cbca01770df838eb6e396dcd3df7bc0380412b8
David Stauffer <dstauffman@yahoo.com>
2015-06-07 20:49:05 -0700

    Add games folder + brick game and exclude results folder.

---
+++ games/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+r"""
+The "games" submodule within the "dstauffman" module is a playground for simulating and solving
+different types of games.
+
+The "brick" file solves a 3D red and gray brick puzzle.
+
+Notes
+-----
+#.  Written by David Stauffer in June 2015.
+"""
+
+#%% Imports
+# None
+
+#%% Unit test
+if __name__ == '__main__':
+    pass
