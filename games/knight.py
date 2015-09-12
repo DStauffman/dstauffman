d236829c68e90426c5d811352a3c734bad2f6843
David Stauffer <dstauffman@yahoo.com>
2015-09-12 10:53:11 -0700

    Initial work on knight.py

---
+++ games/knight.py
@@ -0,0 +1,510 @@
+# -*- coding: utf-8 -*-
+"""
+The "knight" file solves the Knight Board puzzle given to Matt Beck during a job interview.
+
+Notes
+-----
+#.  Written by David C. Stauffer and Matt Beck in September 2015.
+
+[Knight Board]
+The knight board can be represented in x,y coordinates.  The upper left position
+is (0,0) and the bottom right is (7,7).  Assume there is a single knight chess
+piece on the board that can move according to chess rules.  Sample S[tart] and
+E[nd] points are shown below:
+    . . . . . . . .
+    . . . . . . . .
+    . S . . . . . .
+    . . . . . . . .
+    . . . . . E . .
+    . . . . . . . .
+    . . . . . . . .
+    . . . . . . . .
+Level 1: Write a function that accepts a sequence of moves and reports
+    whether the sequence contains only valid knight moves.  It should also
+    optionally print the state of the knight board to the terminal as shown
+    above after each move.  The current position should be marked with a 'K'.
+Level 2: Compute a valid sequence of moves from a given start point to a given
+    end point.
+Level 3: Compute a valid sequence of moves from a given start point to a
+    given end point in the fewest number of moves.
+Level 4: Now repeat the Level 3 task for this 32x32 board.  Also, modify
+    your validator from Level 1 to check your solutions.  This board has the
+    following additional rules:
+        1) W[ater] squares count as two moves when a piece lands there
+        2) R[ock] squares cannot be used
+        3) B[arrier] squares cannot be used AND cannot lie in the path
+        4) T[eleport] squares instantly move you from one T to the other in
+            the same move
+        5) L[ava] squares count as five moves when a piece lands there
+    . . . . . . . . B . . . L L L . . . . . . . . . . . . . . . . .
+    . . . . . . . . B . . . L L L . . . . . . . . . . . . . . . . .
+    . . . . . . . . B . . . L L L . . . L L L . . . . . . . . . . .
+    . . . . . . . . B . . . L L L . . L L L . . . R R . . . . . . .
+    . . . . . . . . B . . . L L L L L L L L . . . R R . . . . . . .
+    . . . . . . . . B . . . L L L L L L . . . . . . . . . . . . . .
+    . . . . . . . . B . . . . . . . . . . . . R R . . . . . . . . .
+    . . . . . . . . B B . . . . . . . . . . . R R . . . . . . . . .
+    . . . . . . . . W B B . . . . . . . . . . . . . . . . . . . . .
+    . . . R R . . . W W B B B B B B B B B B . . . . . . . . . . . .
+    . . . R R . . . W W . . . . . . . . . B . . . . . . . . . . . .
+    . . . . . . . . W W . . . . . . . . . B . . . . . . T . . . . .
+    . . . W W W W W W W . . . . . . . . . B . . . . . . . . . . . .
+    . . . W W W W W W W . . . . . . . . . B . . R R . . . . . . . .
+    . . . W W . . . . . . . . . . B B B B B . . R R . W W W W W W W
+    . . . W W . . . . . . . . . . B . . . . . . . . . W . . . . . .
+    W W W W . . . . . . . . . . . B . . . W W W W W W W . . . . . .
+    . . . W W W W W W W . . . . . B . . . . . . . . . . . . B B B B
+    . . . W W W W W W W . . . . . B B B . . . . . . . . . . B . . .
+    . . . W W W W W W W . . . . . . . B W W W W W W B B B B B . . .
+    . . . W W W W W W W . . . . . . . B W W W W W W B . . . . . . .
+    . . . . . . . . . . . B B B . . . . . . . . . . B B . . . . . .
+    . . . . . R R . . . . B . . . . . . . . . . . . . B . . . . . .
+    . . . . . R R . . . . B . . . . . . . . . . . . . B . T . . . .
+    . . . . . . . . . . . B . . . . . R R . . . . . . B . . . . . .
+    . . . . . . . . . . . B . . . . . R R . . . . . . . . . . . . .
+    . . . . . . . . . . . B . . . . . . . . . . R R . . . . . . . .
+    . . . . . . . . . . . B . . . . . . . . . . R R . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .  # The last four rows originally missing
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+Level 5 [HARD]: Compute the longest sequence of moves to complete Level 3 without
+    visiting the same square twice.  Use the 32x32 board.
+"""
+
+#%% Imports
+# backwards compatibility with v2.7
+from __future__ import print_function
+from __future__ import division
+# regular imports
+import doctest
+from enum import unique
+import numpy as np
+import unittest
+# personal dstauffman libary imports
+from dstauffman import IntEnumPlus
+
+#%% Constants
+CHAR_DICT = {'.':0, 'S':1, 'E':2, 'K':3, 'W':4, 'R':5, 'B':6, 'T':7, 'L':8, 'x': 9}
+BOARD1 = r"""
+. . . . . . . .
+. . . . . . . .
+. S . . . . . .
+. . . . . . . .
+. . . . . E . .
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+"""
+BOARD2 = r"""
+. . . . . . . . B . . . L L L . . . . . . . . . . . . . . . . .
+. . . . . . . . B . . . L L L . . . . . . . . . . . . . . . . .
+. . . . . . . . B . . . L L L . . . L L L . . . . . . . . . . .
+. . . . . . . . B . . . L L L . . L L L . . . R R . . . . . . .
+. . . . . . . . B . . . L L L L L L L L . . . R R . . . . . . .
+. . . . . . . . B . . . L L L L L L . . . . . . . . . . . . . .
+. . . . . . . . B . . . . . . . . . . . . R R . . . . . . . . .
+. . . . . . . . B B . . . . . . . . . . . R R . . . . . . . . .
+. . . . . . . . W B B . . . . . . . . . . . . . . . . . . . . .
+. . . R R . . . W W B B B B B B B B B B . . . . . . . . . . . .
+. . . R R . . . W W . . . . . . . . . B . . . . . . . . . . . .
+. . . . . . . . W W . . . . . . . . . B . . . . . . T . . . . .
+. . . W W W W W W W . . . . . . . . . B . . . . . . . . . . . .
+. . . W W W W W W W . . . . . . . . . B . . R R . . . . . . . .
+. . . W W . . . . . . . . . . B B B B B . . R R . W W W W W W W
+. . . W W . . . . . . . . . . B . . . . . . . . . W . . . . . .
+W W W W . . . . . . . . . . . B . . . W W W W W W W . . . . . .
+. . . W W W W W W W . . . . . B . . . . . . . . . . . . B B B B
+. . . W W W W W W W . . . . . B B B . . . . . . . . . . B . . .
+. . . W W W W W W W . . . . . . . B W W W W W W B B B B B . . .
+. . . W W W W W W W . . . . . . . B W W W W W W B . . . . . . .
+. . . . . . . . . . . B B B . . . . . . . . . . B B . . . . . .
+. . . . . R R . . . . B . . . . . . . . . . . . . B . . . . . .
+. . . . . R R . . . . B . . . . . . . . . . . . . B . T . . . .
+. . . . . . . . . . . B . . . . . R R . . . . . . B . . . . . .
+. . . . . . . . . . . B . . . . . R R . . . . . . . . . . . . .
+. . . . . . . . . . . B . . . . . . . . . . R R . . . . . . . .
+. . . . . . . . . . . B . . . . . . . . . . R R . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+"""
+MOVES = frozenset({-4, -3, -2, -1, 1, 2, 3, 4})
+#MOVES = frozenset({-4, -3, -2, -1, 1, 2, 3, 4, -5, 5, -6, 6, -7, 7, -8, 8})
+# Move design is based on a first direction step of two moves, followed by a single step to the
+# left or right of the first one.
+# Note for chess nerds: The move is two steps and then one.  With no obstructions, it can be thought
+# of as one and then two, but with obstructions, this counts as a different move and is not allowed.
+# The number represents the cardinal direction of the move: 1=North, 2=East, 3=South, 4=West
+# The sign of the number represents whether the second step is left or right (positive is clockwise)
+# Therefore:
+"""
+#  Move -1       Move +1        Move -2      Move +2       Move -3       Move +3       Move -4       Move +4
+# . E x . .  |  . . x E .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .
+# . . x . .  |  . . x . .  |  . . . . E  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  E . . . .
+# . . S . .  |  . . S . .  |  . . S x x  |  . . S x x  |  . . S . .  |  . . S . .  |  x x S . .  |  x x S . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . E  |  . . x . .  |  . . x . .  |  E . . . .  |  . . . . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . x E .  |  . E x . .  |  . . . . .  |  . . . . .
+"""
+
+# Optional extended set?
+"""
+#  Move -5       Move +5        Move -6      Move +6       Move -7       Move +7       Move -8       Move +8
+# . E . . .  |  . . . E .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .
+# . x . . .  |  . . . x .  |  . . x x E  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  E x x . .
+# . x S . .  |  . . S x .  |  . . S . .  |  . . S . .  |  . . S x .  |  . x S . .  |  . . S . .  |  . . S . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . x x E  |  . . . x .  |  . x . . .  |  E x x . .  |  . . . . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . E .  |  . E . . .  |  . . . . .  |  . . . . .
+"""
+NORMAL_COST = 1
+TRANSPORT_COST = 1
+WATER_COST = 2
+LAVA_COST = 5
+INVALID_COST = 0
+
+#%% Classes
+@unique
+class Piece(IntEnumPlus):
+    null      = 0 # Empty square that has never been used
+    start     = 1 # Original starting position
+    final     = 2 # Final ending position
+    current   = 3 # Current knight position
+    water     = 4 # water, costs two moves to land on
+    rock      = 5 # rock, can be jumped across, but not landed on
+    barrier   = 6 # barrier, cannot be landed on or moved across
+    transport = 7 # transports to the other transport, can only be exactly 0 or 2 on the board
+    lava      = 8 # lava, costs 5 moves to land on
+    visited   = 9 # previously visited square that cannot be used again
+
+    @staticmethod
+    def is_valid_place(enumerator):
+        if enumerator in {Piece.null, Piece.final, Piece.water, Piece.transport, Piece.lava}:
+            return True
+        else:
+            return False
+
+#%% print_baord
+def print_board(board):
+    r"""
+    Prints the current board position to the console window.
+    """
+    (rows, cols) = board.shape
+    for i in range(rows):
+        for j in range(cols):
+            print(n2c(board[i,j]) + ' ', end='')
+        print('')
+
+#%% char_board_to_nums
+def char_board_to_nums(char_board):
+    r"""
+    Converts the original board from a character array into a numpy ndarray of int.
+    """
+    # convert to rows of lines
+    lines = char_board.split('\n')
+    # remove any empty rows
+    lines = [this_line for this_line in lines if this_line]
+    rows = len(lines)
+    cols = (len(lines[0]) + 1)//2
+    board = 100*np.ones((rows, cols), dtype=int)
+    for i in range(rows):
+        for j in range(cols):
+            board[i,j] = c2n(lines[i][2*j])
+    return board
+
+#%% n2c
+def n2c(num):
+    r"""
+    Converts from the given integer to a character for printing to the console.
+
+    Parameters
+    ----------
+    num : int
+        Number representing a piece on the board
+
+    Returns
+    -------
+    char : str
+        Single character representing the same piece on the board
+
+    Notes
+    -----
+    #.  Written by David C. Stauffer in September 2015.
+
+    Examples
+    --------
+
+    >>> from dstauffman.games.knight import n2c
+    >>> num  = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    >>> char = [n2c(this_num) for this_num in num]
+    >>> print(char)
+    ['.', 'S', 'E', 'K', 'W', 'R', 'B', 'T', 'L']
+
+    """
+    # determine result based on simple lookup
+    if num == Piece.null:
+        char = '.'
+    elif num == Piece.start:
+        char = 'S'
+    elif num == Piece.final:
+        char = 'E'
+    elif num == Piece.current:
+        char = 'K'
+    elif num == Piece.water:
+        char = 'W'
+    elif num == Piece.rock:
+        char = 'R'
+    elif num == Piece.barrier:
+        char = 'B'
+    elif num == Piece.transport:
+        char = 'T'
+    elif num == Piece.lava:
+        char = 'L'
+    elif num == Piece.visited:
+        char = 'x'
+    else:
+        raise ValueError('Unexpected number = "{}"'.format(num))
+    # return result
+    return char
+
+#%% c2n
+def c2n(char):
+    r"""
+    Converts a given character to an integer to use in an ndarray.
+
+    Parameters
+    ----------
+    char : str
+        Single character representing the same piece on the board
+
+    Returns
+    -------
+    num : int
+        Number representing a piece on the board
+
+    Notes
+    -----
+    #.  Written by David C. Stauffer in September 2015.
+
+    Examples
+    --------
+
+    >>> from dstauffman.games.knight import c2n
+    >>> char = ['.', 'S', 'E', 'K', 'W', 'R', 'B', 'T', 'L']
+    >>> num = [c2n(this_char) for this_char in char]
+    >>> print(num)
+    [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    """
+    num = CHAR_DICT[char]
+    return num
+
+#%% get_current_position
+def get_current_position(board):
+    r"""
+    Gets the current position of the knight.
+    """
+    ix = np.nonzero(board == Piece.current)
+    x = ix[0]
+    y = ix[1]
+    is_valid = len(x) == 1 and len(y) == 1
+    if not is_valid:
+        print_board(board)
+    assert is_valid, 'Only exactly one current position may be found.'
+    return (x, y)
+
+#%% get_new_position
+def get_new_position(x, y, move):
+    r"""
+    Gets the new position of the knight after making the desired move.
+    """
+
+    """
+#  Move -1       Move +1        Move -2      Move +2       Move -3       Move +3       Move -4       Move +4
+# . E x . .  |  . . x E .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .
+# . . x . .  |  . . x . .  |  . . . . E  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  E . . . .
+# . . S . .  |  . . S . .  |  . . S x x  |  . . S x x  |  . . S . .  |  . . S . .  |  x x S . .  |  x x S . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . E  |  . . x . .  |  . . x . .  |  E . . . .  |  . . . . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . x E .  |  . E x . .  |  . . . . .  |  . . . . .
+"""
+    # move the piece
+    if move == -1:
+        pos1 = (x-1, y)
+        pos2 = (x-2, y)
+        pos3 = (x-2, y-1)
+    elif move == 1:
+        pos1 = (x-1, y)
+        pos2 = (x-2, y)
+        pos3 = (x-2, y+1)
+    elif move == -2:
+        pos1 = (x,   y+1)
+        pos2 = (x,   y+2)
+        pos3 = (x-1, y+2)
+    elif move == 2:
+        pos1 = (x,   y+1)
+        pos2 = (x,   y+2)
+        pos3 = (x+1, y+2)
+    elif move == -3:
+        pos1 = (x+1, y)
+        pos2 = (x+2, y)
+        pos3 = (x+2, y+1)
+    elif move == 3:
+        pos1 = (x+1, y)
+        pos2 = (x+2, y)
+        pos3 = (x+2, y-1)
+    elif move == -4:
+        pos1 = (x,   y-1)
+        pos2 = (x,   y-2)
+        pos3 = (x+1, y-2)
+    elif move == 4:
+        pos1 = (x,   y-1)
+        pos2 = (x,   y-2)
+        pos3 = (x-1, y-2)
+    elif move in {-5, 5, -6, 6, -7, 7, -8, 8}:
+        raise ValueError('Extended moves are not yet implemented.')
+    else:
+        raise ValueError('Invalid move of "{}"'.format(move))
+    # return the whole set of stuff
+    return (pos1, pos2, pos3)
+
+#%% move_cost
+def move_cost(board, move):
+    r"""
+    Determines if the desired move is valid, and if so returns a non zero cost.
+
+    A negative cost means the move finishes the puzzle, and the absolute value should be used.
+    """
+    # find the size of the board
+    (xmax, ymax) = board.shape
+    # find current position
+    (x, y) = get_current_position(board)
+    # find the traversal for the desired move
+    (pos1, pos2, pos3) = get_new_position(x, y, move)
+    # first check that pos3 is on the board and if not return
+    if pos3[0] < 0 or pos3[0] > xmax or pos3[1] < 0 or pos3[1] > ymax:
+        return INVALID_COST
+    # check that pos1 and pos2 are on the board
+    pass # TODO: this only matters for non-rectangular boards
+    # get the values for each position
+    p1 = board[pos1[0], pos1[1]][0]
+    p2 = board[pos2[0], pos2[1]][0]
+    p3 = board[pos3[0], pos3[1]][0]
+    # check for valid landing squares
+    if not Piece.is_valid_place(p3):
+        return INVALID_COST
+    # check for barriers in path
+    if p1 == Piece.barrier or p2 == Piece.barrier:
+        return INVALID_COST
+    # move is valid, so now determine the cost
+    if p3 == Piece.null:
+        cost = NORMAL_COST
+    elif p3 == Piece.final:
+        cost = -NORMAL_COST # TODO: can the final piece be something besides a normal empty square?
+    elif p3 == Piece.transport:
+        cost = TRANSPORT_COST
+    elif p3 == Piece.water:
+        cost = WATER_COST
+    elif p3 == Piece.lava:
+        cost = LAVA_COST
+    return cost
+
+#%% update_board
+def update_board(board, move):
+    r"""
+    Updates the new board based on the desired move.
+
+    Notes
+    -----
+    #.  Modifies `board` in-place.
+    """
+    # find current position
+    (x, y) = get_current_position(board)
+    # determine the move cost
+    cost = move_cost(board, move)
+    # if valid, apply the move
+    if cost:
+        # set the current position to visited
+        board[x, y] = Piece.visited
+        # get the new position
+        (_, _, (new_x, new_y)) = get_new_position(x, y, move)
+        # set the new position to current
+        board[new_x, new_y] = Piece.current
+    # return the cost of the update, zero means no update occured
+    return cost
+
+#%% check_valid_sequence
+def check_valid_sequence(board, moves, print_status=False):
+    r"""
+    Checks that the list of moves is a valid sequence to go from start to final position.
+    """
+    # initialize output
+    is_valid = True
+    is_done  = False
+    # create internal board for calculations
+    temp_board = board.copy()
+    # set the current position to the start
+    temp_board[temp_board == Piece.start] = Piece.current
+    for (i, this_move) in enumerate(moves):
+        cost = update_board(temp_board, this_move)
+        if cost == 0:
+            is_valid = False
+            break
+        if cost < 0:
+            is_done = True
+            if i < len(moves)-1:
+                raise ValueError('Sequence finished, but then kept going.')
+    if print_status:
+        if is_valid:
+            if is_done:
+                print('Sequence is valid and finished the puzzle.')
+            else:
+                print('Sequence is valid, but did not finish the puzzle.')
+        else:
+            print('Sequence is not valid.')
+    return is_valid
+
+#%% print_sequence
+def print_sequence(board, moves):
+    # create internal board for calculations
+    temp_board = board.copy()
+    print('Starting position: ')
+    print_board(temp_board)
+    # set the current position to the start
+    temp_board[temp_board == Piece.start] = Piece.current
+    # loop through move sequence
+    for (i,this_move) in enumerate(moves):
+        # print header
+        print('\nAfter move {}'.format(i+1))
+        # update board
+        cost = update_board(temp_board, this_move)
+        # print new board
+        if cost:
+            print_board(temp_board)
+        else:
+            raise ValueError('Bad sequence.')
+
+#%% Unit test
+def main():
+    unittest.main(module='dstauffman.games.test_knight', exit=False)
+    doctest.testmod(verbose=False)
+
+#%% Script
+if __name__ == '__main__':
+    # run unit tests
+    main()
+
+    #%% Solve puzzle
+    # convert board to numeric representation for efficiency
+    board1 = char_board_to_nums(BOARD1)
+    board2 = char_board_to_nums(BOARD2)
+    board3 = np.zeros((5,5), dtype=int)
+    board3[2,2] = Piece.start
+
+    print_board(board1)
+    print_board(board2)
+
+    # Step 1
+    moves1 = [2, 2]
+    is_valid = check_valid_sequence(board1, moves1, print_status=True)
+    if is_valid:
+        print_sequence(board1, moves1)
---
+++ games/test_knight.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+r"""
+Test file for the `games.knight` module of the dstauffman code.  It is intented to contain test
+cases to demonstrate functionaliy and correct outcomes for all the functions within the module.
+
+Notes
+-----
+#.  Written by David C. Stauffer in September 2015.
+"""
+
+#%% Imports
+from __future__ import print_function
+from __future__ import division
+import numpy as np
+import unittest
+from dstauffman import capture_output
+import dstauffman.games.knight as knight
+
+#%% n2c & c2n & CHAR_DICT
+class Test_num_to_char_and_back(unittest.TestCase):
+    r"""
+    Tests the n2c and c2n functions, thus ensuring all the mappings are covered.
+    """
+    def setUp(self):
+        self.nums  = knight.Piece.list_of_values()
+        self.chars = knight.CHAR_DICT.keys()
+
+    def test_c2n(self):
+        for this_char in self.chars:
+            this_num = knight.c2n(this_char)
+            self.assertTrue(this_num in self.nums)
+
+    def test_n2c(self):
+        for this_num in self.nums:
+            this_char = knight.n2c(this_num)
+            self.assertTrue(this_char in self.chars)
+
+    def test_bad_char(self):
+        with self.assertRaises(KeyError):
+            knight.c2n('Z')
+
+    def test_bad_num(self):
+        with self.assertRaises(ValueError):
+            knight.n2c(10000)
+
+#%% print_board
+class Test_print_board(unittest.TestCase):
+    r"""
+    Tests the print_board function with the following cases:
+        Square Board
+        Rectangular board
+        All piece types on the board
+    """
+    def setUp(self):
+        self.board1 = np.ones((4,4), dtype=int)
+        self.board2 = np.zeros((3,5), dtype=int)
+        self.board3 = np.arange(12).reshape((4,3))
+        self.board3[self.board3 > knight.Piece.max_value] = 0
+
+    def test_square_board(self):
+        with capture_output() as (out, _):
+            knight.print_board(self.board1)
+        output = out.getvalue().strip()
+        out.close()
+        self.assertEqual(output, 'S S S S \nS S S S \nS S S S \nS S S S')
+
+    def test_rect_board(self):
+        with capture_output() as (out, _):
+            knight.print_board(self.board2)
+        output = out.getvalue().strip()
+        out.close()
+        self.assertEqual(output, '. . . . . \n. . . . . \n. . . . .')
+
+    def test_all_board_piece_types(self):
+        with capture_output() as (out, _):
+            knight.print_board(self.board3)
+        output = out.getvalue().strip()
+        out.close()
+        self.assertEqual(output, '. S E \nK W R \nB T L \nx . .')
+
+class Test_get_new_position(unittest.TestCase):
+    r"""
+    Tests the get_new_position function with the following cases:
+        All valid moves
+        Not yet done moves
+        Invalid moves
+    """
+
+#  Move -1       Move +1        Move -2      Move +2       Move -3       Move +3       Move -4       Move +4
+# . E x . .  |  . . x E .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .
+# . . x . .  |  . . x . .  |  . . . . E  |  . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  E . . . .
+# . . S . .  |  . . S . .  |  . . S x x  |  . . S x x  |  . . S . .  |  . . S . .  |  x x S . .  |  x x S . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . E  |  . . x . .  |  . . x . .  |  E . . . .  |  . . . . .
+# . . . . .  |  . . . . .  |  . . . . .  |  . . . . .  |  . . x E .  |  . E x . .  |  . . . . .  |  . . . . .
+    def setUp(self):
+        self.x = 2
+        self.y = 2
+        self.board = np.zeros((5,5), dtype=int)
+        self.board[self.x, self.y] = knight.Piece.start
+        self.valid_moves = [-4, -3, -2, -1, 1, 2, 3, 4]
+        self.future_moves = [-8, -7, -6, -5, 5, 6, 7, 8]
+        self.bad_moves = [0, 9, -9, 100]
+        self.results = [(3, 0), (4, 3), (1, 4), (0, 1), (0, 3), (3, 4), (4, 1), (1, 0)]
+
+    def test_valid_moves(self):
+        for (this_move, this_result) in zip(self.valid_moves, self.results):
+            (pos1, pos2, pos3) = knight.get_new_position(self.x, self.y, this_move)
+            self.assertEqual(pos3, this_result)
+            # TODO: assert something about pos1 and pos2?
+
+    def test_future_moves(self):
+        for (this_move, this_result) in zip(self.future_moves, self.results):
+            with self.assertRaises(ValueError):
+                knight.get_new_position(self.x, self.y, this_move)
+            #(pos1, pos2, pos3) = knight.get_new_position(self.x, self.y, this_move)
+            #self.assertEqual(pos3, this_result)
+
+    def test_bad_moves(self):
+        for this_move in self.bad_moves:
+            with self.assertRaises(ValueError):
+                knight.get_new_position(self.x, self.y, this_move)
+
+#%% char_board_to_nums
+pass # TODO: write this
+
+#%% Unit test execution
+if __name__ == '__main__':
+    unittest.main(exit=False)
