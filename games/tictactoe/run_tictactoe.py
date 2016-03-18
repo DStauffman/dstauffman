7b85d16f1bdca1a0d671355db70596a633497b53
David Stauffer <dstauffman@yahoo.com>
2016-03-18 15:58:18 -0700

    Potential script for building the GUI into a stand-alone app.
    I haven't been able to get this working yet.

---
+++ games/tictactoe/run_tictactoe.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+r"""
+Runs the Tic Tac Toe GUI.
+
+Written by David C. Stauffer in March 2016.
+
+"""
+# Build with:
+# pyinstaller --onefile --windowed run_tictactoe.py
+
+#%% Imports
+import sys
+try:
+    from PyQt5.QtWidgets import QApplication
+except ImportError:
+    from PyQt4.QtGui import QApplication
+from dstauffman.games.tictactoe import TicTacToeGui
+
+#%% Execution
+if __name__ == '__main__':
+    # Runs the GUI application
+    qapp = QApplication(sys.argv)
+    # instatiates the GUI
+    gui = TicTacToeGui()
+    gui.show()
+    # exits and returns the code on close of all main application windows
+    sys.exit(qapp.exec_())
