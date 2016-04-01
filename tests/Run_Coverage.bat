601d3feb16ccdad4173716dd8c05f8cd2e99cf1e
David Stauffer <dstauffman@yahoo.com>
2016-03-31 18:05:32 -0700

    Put pause at end of coverage testing.

--- tests/Run_Coverage.bat
+++ tests/Run_Coverage.bat
@@ -2,3 +2,6 @@ cd "C:\Users\%username%\Documents\GitHub\dstauffman\tests
 coverage run run_all_tests.py
 coverage html
 start coverage_html_report\index.html
+
+echo 'Press any key to continue'
+pause
