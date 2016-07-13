00b0c3698e892f5ff6ce52c06159fca549c04be3
David Stauffer <dstauffman@yahoo.com>
2016-07-13 14:38:22 -0700

    Update autodoc.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'dstauffman'
-copyright = '2015, David C. Stauffer'
+copyright = '2016, David C. Stauffer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
