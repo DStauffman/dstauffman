d550810d2b811a19aee298da32c644808ecdaec0
David Stauffer <dstauffman@yahoo.com>
2017-03-23 12:03:23 -0700

    Minor clean-up.

--- __init__.py
+++ __init__.py
@@ -11,32 +11,31 @@ Notes
 """
 
 #%% Imports
-from .bpe        import Logger, OptiOpts, OptiParam, BpeResults, CurrentResults, \
-                            validate_opti_opts, run_bpe, plot_bpe_results
-from .classes    import Frozen, SaveAndLoad, SaveAndLoadPickle, Counter, FixedDict
-from .constants  import MONTHS_PER_YEAR, INT_TOKEN, DEFAULT_COLORMAP, QUAT_SIZE
-from .enums      import IntEnumPlus, consecutive, dist_enum_and_mons
-from .latex      import make_preamble, make_conclusion, bins_to_str_ranges
-from .linalg     import orth, subspace
-from .plotting   import Plotter, Opts, TruthPlotter, MyCustomToolbar, ColorMap, close_all, \
-                            get_axes_scales, plot_time_history, plot_correlation_matrix, \
-                            plot_multiline_history, plot_bar_breakdown, plot_bpe_convergence, \
-                            storefig, titleprefix, disp_xlimits, setup_plots, figmenu, \
-                            rgb_ints_to_hex
-from .quat       import qrot, quat_angle_diff, quat_from_euler, quat_interp, quat_inv, quat_mult, \
-                            quat_norm, quat_prop, quat_times_vector, quat_to_dcm, quat_to_euler
-from .stats      import convert_annual_to_monthly_probability, \
-                            convert_monthly_to_annual_probability, ca2mp, cm2ap, prob_to_rate, \
-                            rate_to_prob, month_prob_mult_ratio, \
-                            annual_rate_to_monthly_probability, \
-                            monthly_probability_to_annual_rate, ar2mp, mp2ar, combine_sets, icer
-from .units      import Units, get_factors
-from .utils      import rms, rss, setup_dir, compare_two_classes, compare_two_dicts, round_time, \
-                            make_python_init, get_python_definitions, read_text_file, \
-                            write_text_file, get_root_dir, get_tests_dir, get_data_dir, \
-                            get_images_dir, get_output_dir, capture_output, unit, reload_package, \
-                            delete_pyc, rename_module, modd, find_tabs, np_digitize, full_print, \
-                            pprint_dict
+from .bpe       import Logger, OptiOpts, OptiParam, BpeResults, CurrentResults, \
+                           validate_opti_opts, run_bpe, plot_bpe_results
+from .classes   import Frozen, SaveAndLoad, SaveAndLoadPickle, Counter, FixedDict
+from .constants import MONTHS_PER_YEAR, INT_TOKEN, DEFAULT_COLORMAP, QUAT_SIZE
+from .enums     import IntEnumPlus, consecutive, dist_enum_and_mons
+from .latex     import make_preamble, make_conclusion, bins_to_str_ranges
+from .linalg    import orth, subspace
+from .plotting  import Plotter, Opts, TruthPlotter, MyCustomToolbar, ColorMap, close_all, \
+                           get_axes_scales, plot_time_history, plot_correlation_matrix, \
+                           plot_multiline_history, plot_bar_breakdown, plot_bpe_convergence, \
+                           storefig, titleprefix, disp_xlimits, setup_plots, figmenu, \
+                           rgb_ints_to_hex
+from .quat      import qrot, quat_angle_diff, quat_from_euler, quat_interp, quat_inv, quat_mult, \
+                           quat_norm, quat_prop, quat_times_vector, quat_to_dcm, quat_to_euler
+from .stats     import convert_annual_to_monthly_probability, \
+                           convert_monthly_to_annual_probability, prob_to_rate, rate_to_prob, \
+                           month_prob_mult_ratio, annual_rate_to_monthly_probability, \
+                           monthly_probability_to_annual_rate, combine_sets, icer
+from .units     import Units, get_factors
+from .utils     import rms, rss, setup_dir, compare_two_classes, compare_two_dicts, round_time, \
+                           make_python_init, get_python_definitions, read_text_file, \
+                           write_text_file, get_root_dir, get_tests_dir, get_data_dir, \
+                           get_images_dir, get_output_dir, capture_output, unit, reload_package, \
+                           delete_pyc, rename_module, modd, find_tabs, np_digitize, full_print, \
+                           pprint_dict
 
 #%% Unit test
 if __name__ == '__main__':
