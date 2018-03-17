c26f62c8544f295cbba95ea6c39874a39b5b64a4
David Stauffer <dstauffman@yahoo.com>
2018-03-17 13:35:16 -0700

    Use pytest instead of unittest.

--- dstauffman/tests/run_all_tests.py
+++ dstauffman/tests/run_all_tests.py
@@ -8,8 +8,8 @@ Notes
 """
 
 #%% Imports
+import pytest
 import sys
-import unittest
 
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication
@@ -23,11 +23,6 @@ if __name__ == '__main__':
         qapp = QApplication(sys.argv)
     else:
         qapp = QApplication.instance()
-    # get a loader
-    loader = unittest.TestLoader()
-    # find all the test cases
-    test_suite = loader.discover('dstauffman.tests')
-    # run the tests
-    unittest.TextTestRunner(verbosity=1).run(test_suite)
+    pytest.main(['-k', 'tests'])
     # close the qapp
     qapp.closeAllWindows()
--- dstauffman/tests/test_linalg.py
+++ dstauffman/tests/test_linalg.py
@@ -9,20 +9,20 @@ Notes
 """
 
 #%% Imports
-import unittest
+import pytest
 
 import numpy as np
 
 import dstauffman as dcs
 
 #%% orth
-class Test_orth(unittest.TestCase):
+class Test_orth():
     r"""
     Tests the orth function with the following cases:
         Rank 3 matrix
         Rank 2 matrix
     """
-    def setUp(self):
+    def setup(self):
         self.A1 = np.array([[1, 0, 1], [-1, -2, 0], [0, 1, -1]])
         self.r1 = 3
         self.Q1 = np.array([[-0.12000026, -0.80971228, 0.57442663], [ 0.90175265, 0.15312282, \
@@ -33,23 +33,23 @@ class Test_orth(unittest.TestCase):
 
     def test_rank3(self):
         r = np.linalg.matrix_rank(self.A1)
-        self.assertEqual(r, self.r1)
+        assert r == self.r1
         Q = dcs.orth(self.A1)
-        np.testing.assert_array_almost_equal(Q, self.Q1)
+        assert Q == pytest.approx(self.Q1)
 
     def test_rank2(self):
         r = np.linalg.matrix_rank(self.A2)
-        self.assertEqual(r, self.r2)
+        assert r == self.r2
         Q = dcs.orth(self.A2)
-        np.testing.assert_array_almost_equal(Q, self.Q2)
+        assert Q == pytest.approx(self.Q2)
 
 #%% subspace
-class Test_subspace(unittest.TestCase):
+class Test_subspace():
     r"""
     Tests the subspace function with the followinc cases:
         Nominal
     """
-    def setUp(self):
+    def setup(self):
         self.A = np.array([[1, 1, 1], [-1, 1, -1], [1, -1, -1], [-1, -1, 1], [1, 1, 1], [-1, 1, -1], \
             [1, -1, -1], [-1, -1, 1]])
         self.B = np.array([[1, 1, 1, 1], [1, -1, 1, -1],[1, 1, -1, -1], [1, -1, -1, 1], [-1, -1, -1, -1], \
@@ -58,12 +58,12 @@ class Test_subspace(unittest.TestCase):
 
     def test_nominal(self):
         theta = dcs.subspace(self.A, self.B)
-        self.assertAlmostEqual(theta, self.theta)
+        assert theta == pytest.approx(self.theta)
 
     def test_swapped_rank(self):
         theta = dcs.subspace(self.B, self.A)
-        self.assertAlmostEqual(theta, self.theta)
+        assert theta == pytest.approx(self.theta)
 
 #%% Unit test execution
 if __name__ == '__main__':
-    unittest.main(exit=False)
+    pytest.main(['-k','test_linalg.py'])
