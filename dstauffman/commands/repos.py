90330159fd0b1de0601d1c1ee804b24bd1574675
David Stauffer <david.c.stauffer@lmco.com>
2020-03-20 11:12:17 -0700

    Update the enforce command to wrap find_repo_issues function.

--- dstauffman/commands/repos.py
+++ dstauffman/commands/repos.py
@@ -10,6 +10,7 @@ Notes
 #%% Imports
 import argparse
 import doctest
+import os
 import unittest
 
 from dstauffman.enums import ReturnCodes
@@ -32,14 +33,29 @@ def parse_enforce(input_args):
 
     Examples
     --------
+    >>> from dstauffman import get_root_dir
     >>> from dstauffman.commands import parse_enforce
-    >>> input_args = []
+    >>> input_args = [get_root_dir()]
     >>> args = parse_enforce(input_args)
-    >>> print(args)
-    Namespace()
+    >>> print(args) # doctest: +ELLIPSIS
+    Namespace(execute=False, extensions=None, folder='...', ignore_tabs=False, list_all=False, skip=None, trailing=False, unix=False, windows=False)
 
     """
-    parser = argparse.ArgumentParser(prog='lms enforce')
+    parser = argparse.ArgumentParser(prog='dcs enforce', description='Enforce consistency in the repo ' + \
+            'for things like tabs, trailing whitespace, line endings and file execute permissions.')
+
+    parser.add_argument('folder', help='Folder to search for source files')
+    parser.add_argument('-e', '--extensions', help='Extensions to search through.', action='append')
+    parser.add_argument('-l', '--list-all', help='List all files, even ones without problems.', action='store_true')
+    parser.add_argument('-i', '--ignore-tabs', help='Ignore tabs within the source code.', action='store_true')
+    parser.add_argument('-t', '--trailing', help='Show files with trailing whitespace', action='store_true')
+    parser.add_argument('-s', '--skip', help='Exclusions to not search.', action='append')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-w', '--windows', help='Use Windows (CRLF) line-endings', action='store_true')
+    group.add_argument('-u', '--unix', help='Use Unix (LF) line-endings', action='store_true')
+
+    parser.add_argument('-x', '--execute', help='List files with execute permissions.', action='store_true')
 
     args = parser.parse_args(input_args)
     return args
@@ -61,12 +77,42 @@ def execute_enforce(args):
 
     Examples
     --------
+    >>> from dstauffman import get_root_dir
     >>> from dstauffman.commands import execute_enforce
-    >>> args = []
-    >>> execute_enforce(args) # doctest: +SKIP
+    >>> from argparse import Namespace
+    >>> args = Namespace(extensions=None, folder=get_root_dir(), ignore_tabs=False, list_all=False, \
+    ...     skip=None, trailing=False, unix=False, windows=False, execute=False)
+    >>> return_code = execute_enforce(args) # doctest: +SKIP
 
     """
-    return_code = ReturnCodes.clean
+    # defaults
+    def_extensions = {'m', 'py'}
+    # get settings from input arguments
+    folder     = os.path.abspath(args.folder)
+    list_all   = args.list_all
+    check_tabs = not args.ignore_tabs
+    trailing   = args.trailing
+    exclusions = args.skip
+    show_execute = args.execute
+    if args.windows:
+        check_eol = '\r\n'
+    elif args.unix:
+        check_eol = '\n'
+    else:
+        check_eol = None
+    if args.extensions is None:
+        extensions = frozenset(def_extensions)
+    elif len(args.extensions) == 1 and args.extensions[0] == '*':
+        extensions = None
+    else:
+        extensions = args.extensions
+
+    # call the function to do the checks
+    is_clean = find_repo_issues(folder=folder, extensions=extensions, list_all=list_all, \
+            check_tabs=check_tabs, trailing=trailing, exclusions=exclusions, check_eol=check_eol, \
+            show_execute=show_execute)
+    # return a status based on whether anything was found
+    return_code = ReturnCodes.clean if is_clean else ReturnCodes.test_failures
     return return_code
 
 #%% Unit test
