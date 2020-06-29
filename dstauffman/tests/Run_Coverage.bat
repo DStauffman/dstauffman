139262a87994a2a823be7a7f74f68ef1bde2420a
David Stauffer <dstauffman@yahoo.com>
2020-06-29 10:09:05 -0700

    Update Coverage bat file.

--- dstauffman/tests/Run_Coverage.bat
+++ dstauffman/tests/Run_Coverage.bat
@@ -11,13 +11,13 @@ REM Save the original paths
 SET PYTHONPATH_ORIG=%PYTHONPATH%
 
 REM Modify as needed, currently only modifying the python path, note replacing the whole contents, not appending
-SET PYTHONPATH=%tests_home1%\..\..
+SET PYTHONPATH=%tests_home%\..\..
 
 REM Run the coverage tool and generate the report
 REM TODO: this first option doesn't collect the statistics correctly
 REM coverage run -m dstauffman tests
-coverage run %tests_home%\run_all_tests.py
-coverage html
+call coverage run %tests_home%\run_all_tests.py
+call coverage html
 
 REM Set the path back to orignals
 SET PYTHONPATH=%PYHONPATH_ORIG%
@@ -25,4 +25,5 @@ SET PYTHONPATH=%PYHONPATH_ORIG%
 REM Open the report
 start coverage_html_report\index.html
 
-pause
+echo Press any key to continue . . .
+pause >nul
