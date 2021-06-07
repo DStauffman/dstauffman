e6fb70d8e2e7dbd057412e45e2d13fed9f4c8946
David Stauffer <david.c.stauffer@lmco.com>
2021-06-07 16:12:55 -0700

    Update numba and PyQt5 checks for consistency.

--- dstauffman/numba/__init__.py
+++ dstauffman/numba/__init__.py
@@ -9,7 +9,7 @@ Notes
 #%% Imports
 from .numpy_mods  import issorted_ascend, issorted_descend, np_all_axis0, np_all_axis1, \
                              np_any_axis0, np_any_axis1
-from .passthrough import fake_jit, ncjit, TARGET
+from .passthrough import fake_jit, HAVE_NUMBA, ncjit, TARGET
 from .optimized   import np_any, np_all, issorted_opt, prob_to_rate_opt, rate_to_prob_opt, \
                              zero_divide
 
